3954a020493a5f714dc0a1c986e95ee8af7a2b1c
RogerSouza2606 <rogersouza2606@gmail.com>
2023-09-27 17:40:41 -0300

    Cadastro de Projetos(Produtos)

--- cadastroDeProjetos/src/main/java/br/com/rogerio/controller/ProjetoController.java
+++ cadastroDeProjetos/src/main/java/br/com/rogerio/controller/ProjetoController.java
@@ -3,41 +3,70 @@ package br.com.rogerio.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.rogerio.model.Projeto;
 import br.com.rogerio.repository.ProjetoRepository;
 
-@Controller
-@ResponseBody
+@RestController
+@RequestMapping("/projetos")
 public class ProjetoController {
 	
-	/*@GetMapping("/olamundo")
-	public String olaMundo() {
-		return "Olá Mundo";
-	}*/
-	
-	@Autowired
-	private ProjetoRepository projetoRepository;
-	
-	@GetMapping("/listarProjetos")
-	public List<Projeto> listarProjetos() {
-		/*Projeto p1 = new Projeto(35, 1299.99, "Celular Sansung");
-		Projeto p2 = new Projeto(10, 199.99, "Cafeteria arno");
-		Projeto p3 = new Projeto(150, 9.99, "Rato sem fio");
+
+		/*@GetMapping("/olamundo")
+		public String olaMundo() {
+			return "Olá Mundo";
+		}*/
 		
-		return Arrays.asList(p1, p2, p3);*/
-		List<Projeto> projetos = projetoRepository.findAll();
-		return projetos;
+		@Autowired
+		private ProjetoRepository projetoRepository;
+		
+		@GetMapping
+		public List<Projeto> listarProdutos() {
+			/*Produto p1 = new Produto("Celular sansuga", 35, 1299.99);
+			Produto p2 = new Produto("Cafeteira arno", 10, 199.99);
+			Produto p3 = new Produto("Rato sem fio", 150, 9.95);
+			
+			return Arrays.asList(p1, p2, p3);*/
+			List<Projeto> produtos = projetoRepository.findAll();
+			return produtos;
+		}
+		
+		@GetMapping("/{id}")
+		public ResponseEntity<Projeto> findById(@PathVariable Long id) {
+			return projetoRepository.findById(id)
+					.map(objetoGravado -> ResponseEntity.ok().body(objetoGravado))
+					.orElse(ResponseEntity.notFound().build());
+		}		
+		
+
+	@PostMapping
+	public ResponseEntity<Projeto> cadastroProjeto(@RequestBody Projeto projeto){
+		return ResponseEntity.status(HttpStatus.CREATED).body(projeto);
+}		
+	@PutMapping("/{id}")
+	public ResponseEntity<Projeto> atualizarProjetos(@PathVariable Long id, @RequestBody Projeto projeto) {
+		return projetoRepository.findById(id)
+				.map(objetoGravado -> {
+					objetoGravado.setNome(projeto.getNome());
+					objetoGravado.setQuantidade(projeto.getQuantidade());
+					objetoGravado.setPreco(projeto.getPreco());
+					Projeto projetoAtualizado = projetoRepository.save(objetoGravado);
+					return ResponseEntity.ok().body(projetoAtualizado);   
+				})
+				.orElse(ResponseEntity.notFound().build());
+		
 	}
-	
-	@GetMapping("/{id}")
-	public ResponseEntity<Projeto> finnById(@PathVariable Long id) {
-		return projetoRepository.findById(id).map(objetoGravado -> ResponseEntity.ok().body(objetoGravado)).orElse(ResponseEntity.notFound().build());
-	}
-	
 }
+	
--- cadastroDeProjetos/src/main/java/br/com/rogerio/model/Projeto.java
+++ cadastroDeProjetos/src/main/java/br/com/rogerio/model/Projeto.java
@@ -20,35 +20,43 @@ public class Projeto {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public int getQuantidade() {
-		return quantidade;
-	}
-	public void setQuantidade(int quantidade) {
-		this.quantidade = quantidade;
-	}
-	public double getPreco() {
-		return preco;
-	}
-	public void setPreco(double preco) {
-		this.preco = preco;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
 	
 	public Projeto(int quantidade, double preco, String nome) {
 		super();
 		this.quantidade = quantidade;
 		this.preco = preco;
+		this.nome = nome;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
 		this.nome = nome;
 	}
 	
