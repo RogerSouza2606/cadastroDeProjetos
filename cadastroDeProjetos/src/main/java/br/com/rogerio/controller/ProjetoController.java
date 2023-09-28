14ac482213018f433934ac54c32d31fbae03469b
RogerSouza2606 <rogersouza2606@gmail.com>
2023-09-28 14:52:22 -0300

    Cadastro de Projetos(Produtos)

--- cadastroDeProjetos/src/main/java/br/com/rogerio/controller/ProjetoController.java
+++ cadastroDeProjetos/src/main/java/br/com/rogerio/controller/ProjetoController.java
@@ -7,6 +7,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,37 +25,38 @@ import br.com.rogerio.repository.ProjetoRepository;
 public class ProjetoController {
 	
 
-		/*@GetMapping("/olamundo")
-		public String olaMundo() {
-			return "Olá Mundo";
-		}*/
+	/*@GetMapping("/olamundo")
+	public String olaMundo() {
+		return "Olá Mundo";
+	}*/
+	
+	@Autowired
+	private ProjetoRepository projetoRepository;
+	
+	@GetMapping
+	public List<Projeto> listarProjetos() {
+		/*Produto p1 = new Produto("Celular sansuga", 35, 1299.99);
+		Produto p2 = new Produto("Cafeteira arno", 10, 199.99);
+		Produto p3 = new Produto("Rato sem fio", 150, 9.95);
 		
-		@Autowired
-		private ProjetoRepository projetoRepository;
-		
-		@GetMapping
-		public List<Projeto> listarProdutos() {
-			/*Produto p1 = new Produto("Celular sansuga", 35, 1299.99);
-			Produto p2 = new Produto("Cafeteira arno", 10, 199.99);
-			Produto p3 = new Produto("Rato sem fio", 150, 9.95);
-			
-			return Arrays.asList(p1, p2, p3);*/
-			List<Projeto> produtos = projetoRepository.findAll();
-			return produtos;
-		}
-		
-		@GetMapping("/{id}")
-		public ResponseEntity<Projeto> findById(@PathVariable Long id) {
-			return projetoRepository.findById(id)
-					.map(objetoGravado -> ResponseEntity.ok().body(objetoGravado))
-					.orElse(ResponseEntity.notFound().build());
-		}		
-		
-
+		return Arrays.asList(p1, p2, p3);*/
+		List<Projeto> projetos = projetoRepository.findAll();
+		return projetos;
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Projeto> findById(@PathVariable Long id) {
+		return projetoRepository.findById(id)
+				.map(objetoGravado -> ResponseEntity.ok().body(objetoGravado))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
 	@PostMapping
-	public ResponseEntity<Projeto> cadastroProjeto(@RequestBody Projeto projeto){
-		return ResponseEntity.status(HttpStatus.CREATED).body(projeto);
-}		
+	public ResponseEntity<Projeto> cadastrarProjeto(@RequestBody Projeto projeto) {
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(projetoRepository.save(projeto));
+	}
+	
 	@PutMapping("/{id}")
 	public ResponseEntity<Projeto> atualizarProjetos(@PathVariable Long id, @RequestBody Projeto projeto) {
 		return projetoRepository.findById(id)
@@ -68,5 +70,13 @@ public class ProjetoController {
 				.orElse(ResponseEntity.notFound().build());
 		
 	}
-}
 	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deletarProjeto(@PathVariable long id) {
+		return projetoRepository.findById(id)
+		.map(objetoGravado -> {
+			projetoRepository.deleteById(id);
+			return ResponseEntity.noContent().<Void>build();
+		}).orElse(ResponseEntity.notFound().build());
+	}
+}
